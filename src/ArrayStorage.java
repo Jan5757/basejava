093627ee9d9ce92fd95afab67eb5a84f311a002b
Jan <msconfig555@gmail.com>
2022-05-13 14:43:53 +0300

    corrections from Max 2 (fix countResume)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,40 +5,41 @@ import java.util.Objects;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
-    private int CountResume = 0;
+    private int countResume = 0;
 
     void clear() {
-        for (int i = 0; i < CountResume; i++) {
+        for (int i = 0; i < countResume; i++) {
             storage[i] = null;
         }
-        CountResume = 0;
+        countResume = 0;
     }
 
     //if the same "Unique identifier", the resume is replaced
     void save(Resume r) {
         Objects.requireNonNull(r, "Резюме не должно быть пустым (null)");
-        for (int i = 0; i < CountResume; i++) {
+        for (int i = 0; i < countResume; i++) {
             if (storage[i].uuid.equals(r.uuid)) {
                 storage[i] = r;
                 return;
             }
         }
-        storage[CountResume] = r;
-        CountResume++;
+        storage[countResume] = r;
+        countResume++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < CountResume; i++) {
+        for (int i = 0; i < countResume; i++) {
             if (storage[i].uuid.equals(uuid)) return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < CountResume; i++) {
+        for (int i = 0; i < countResume; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[CountResume - 1];
-                storage[CountResume - 1] = null;
+                storage[i] = storage[countResume - 1];
+                storage[countResume - 1] = null;
+                countResume--;
                 return;
             }
         }
@@ -48,13 +49,13 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[CountResume];
-        System.arraycopy(storage, 0, resumes, 0, CountResume);
+        Resume[] resumes = new Resume[countResume];
+        System.arraycopy(storage, 0, resumes, 0, countResume);
         return resumes;
     }
 
     int size() {
-        return CountResume;
+        return countResume;
     }
 
 }
