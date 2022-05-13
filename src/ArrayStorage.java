f6e73cf7791a13eca3ffc2114046afb47fdca76c
Jan <msconfig555@gmail.com>
2022-05-13 12:00:53 +0300

    realized methods (save, get, delete, clear, getAll, size)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,79 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int firstIndexOfNullResume = 0;
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            } else {
+                firstIndexOfNullResume = 0;
+                return;
+            }
+        }
     }
 
+    //if the same "Unique identifier", the resume is replaced
     void save(Resume r) {
+        Objects.requireNonNull(r, "Резюме не должно быть пустым (null)");
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                if (storage[i].uuid.equals(r.uuid)) {
+                    storage[i] = r;
+                    return;
+                } else if (i == storage.length - 1) {
+                    resize();
+                }
+            } else {
+                storage[i] = r;
+                firstIndexOfNullResume++;
+                return;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < firstIndexOfNullResume; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < firstIndexOfNullResume; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                swapBeforeLastNull(i);
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[firstIndexOfNullResume];
+        System.arraycopy(storage, 0, result, 0, firstIndexOfNullResume);
+        return result;
     }
 
     int size() {
-        return 0;
+        return firstIndexOfNullResume;
+    }
+
+    private void resize() {
+        Resume[] newStorage = new Resume[storage.length * 2];
+        System.arraycopy(storage, 0, newStorage, 0, storage.length);
+        storage = newStorage;
+    }
+
+    private void swapBeforeLastNull(int index) {
+        storage[index] = storage[firstIndexOfNullResume - 1];
+        storage[firstIndexOfNullResume - 1] = null;
     }
 }
